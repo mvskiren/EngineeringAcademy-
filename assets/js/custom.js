e871b0892f7a24348e38e08bff32c7dc7a0a4280
MvsKiran <kiranmvs07@gmail.com>
2018-01-06 13:01:10 +0530

    added all new files

---
+++ assets/js/custom.js
@@ -0,0 +1,166 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        scrollAnimation_fun: function () {
+
+            /*====================================
+             ON SCROLL ANIMATION SCRIPTS
+            ======================================*/
+
+
+            window.scrollReveal = new scrollReveal();
+
+        },
+         scroll_fun: function () {
+
+            /*====================================
+                 EASING PLUGIN SCRIPTS
+                ======================================*/
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+
+        },
+
+         top_flex_slider_fun:function()
+         {
+             /*====================================
+              FLEX SLIDER SCRIPTS
+             ======================================*/
+             $('#main-section').flexslider({
+                 animation: "fade", //String: Select your animation type, "fade" or "slide"
+                 slideshowSpeed: 3000,           //Integer: Set the speed of the slideshow cycling, in milliseconds
+                 animationSpeed: 1000,           //Integer: Set the speed of animations, in milliseconds
+                 startAt: 0,    //Integer: The slide that the slider should start on. Array notation (0 = first slide)
+
+             });
+         },
+
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+
+
+    $(document).ready(function () {
+        mainApp.scrollAnimation_fun();
+        mainApp.scroll_fun();
+        mainApp.top_flex_slider_fun();
+        mainApp.custom_fun();
+    });
+}(jQuery));
+
+$(document).ready(function () {
+
+    $(".next-step").click(function (e) {
+
+        var $active = $('.nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var $active = $('.nav-tabs li.active');
+        prevTab($active);
+
+    });
+});
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
+
+
+$(document).ready(function(){
+
+$(function(){
+
+    $(document).on( 'scroll', function(){
+
+    	if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+});
+
+function scrollToTop() {
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	element = $('body');
+	offset = element.offset();
+	offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+
+});
+
+function clock() {// We create a new Date object and assign it to a variable called "time".
+var time = new Date(),
+
+    // Access the "getHours" method on the Date object with the dot accessor.
+    hours = time.getHours(),
+
+    // Access the "getMinutes" method with the dot accessor.
+    minutes = time.getMinutes(),
+
+
+    seconds = time.getSeconds();
+
+document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+
+  function harold(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn
+    }
+    return standIn;
+  }
+}
+setInterval(clock, 1000);
+
+
+
+(function($) {
+
+	var tabs =  $(".tabs li a");
+
+	tabs.click(function() {
+		var content = this.hash.replace('/','');
+		tabs.removeClass("active");
+		$(this).addClass("active");
+    $("#content").find('p').hide();
+    $(content).fadeIn(200);
+	});
+
+})(jQuery);
